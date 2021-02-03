534317df0da31715a0d3495fb6ee12eb750d88a1
Wilmer Arambula <terabytesoftw@gmail.com>
2021-02-03 07:06:43 -0300

    Clean up and refactoring ListView::class.

--- src/ListView.php
+++ src/ListView.php
@@ -5,104 +5,69 @@ declare(strict_types=1);
 namespace Yiisoft\Yii\DataView;
 
 use Closure;
+use InvalidArgumentException;
+use Throwable;
+use Yiisoft\Aliases\Aliases;
 use Yiisoft\Arrays\ArrayHelper;
+use Yiisoft\Data\Paginator\PaginatorInterface;
 use Yiisoft\Html\Html;
+use Yiisoft\Translator\TranslatorInterface;
 use Yiisoft\View\ViewContextInterface;
+use Yiisoft\View\WebView;
 use Yiisoft\Yii\DataView\Exception\InvalidConfigException;
 
-/**
- * The ListView widget is used to display data from data
- * provider. Each data model is rendered using the view
- * specified.
- * For more details and usage information on ListView, see the [guide article on data
- * widgets](guide:output-data-widgets).
- */
-class ListView extends BaseListView implements ViewContextInterface
-{
-    /**
-     * @var array|Closure the HTML attributes for the container of the rendering result of each data model.
-     *                    This can be either an array specifying the common HTML attributes for rendering each data
-     *     item, or an anonymous function that returns an array of the HTML attributes. The anonymous function will be
-     *     called once for every data model returned by [[dataProvider]]. The "tag" element specifies the tag name of
-     *     the container element and defaults to "div". If "tag" is false, it means no container element will be
-     *     rendered. If this property is specified as an anonymous function, it should have the following signature:
-     * ```php
-     * function ($model, $key, $index, $widget)
-     * ```
-     *
-     * @see \Yiisoft\Html\Html::renderTagAttributes() for details on how attributes are being rendered.
-     */
-    private $itemOptions = [];
-    /**
-     * @var callable|string|null the name of the view for rendering each data item, or a callback (e.g. an anonymous
-     *     function) for rendering each data item. If it specifies a view name, the following variables will be
-     *     available in the view:
-     * - `$model`: mixed, the data model
-     * - `$key`: mixed, the key value associated with the data item
-     * - `$index`: integer, the zero-based index of the data item in the items array returned by [[dataProvider]].
-     * - `$widget`: ListView, this widget instance
-     * Note that the view name is resolved into the view file by the current context of the [[view]] object.
-     * If this property is specified as a callback, it should have the following signature:
-     * ```php
-     * function ($model, $key, $index, $widget)
-     * ```
-     */
-    private $itemView;
+use function array_keys;
+use function array_merge;
+use function array_values;
+use function call_user_func;
+use function is_array;
+use function is_callable;
+use function is_string;
 
-    /**
-     * @var array additional parameters to be passed to [[itemView]] when it is being rendered.
-     *            This property is used only when [[itemView]] is a string representing a view name.
-     */
-    private array $viewParams = [];
-    /**
-     * @var string the HTML code to be displayed between any two consecutive items.
-     */
-    private string $separator = "\n";
-    /**
-     * @var array the HTML attributes for the container tag of the list view.
-     *            The "tag" element specifies the tag name of the container element and defaults to "div".
-     *
-     * @see \Yiisoft\Html\Html::renderTagAttributes() for details on how attributes are being rendered.
-     */
+/**
+ * The ListView widget is used to display data from data provider. Each data model is rendered using the view specified.
+ *
+ * For more details and usage information on ListView:
+ * see the [guide article on data widgets](guide:output-data-widgets).
+ */
+final class ListView extends BaseListView implements ViewContextInterface
+{
     protected array $options = ['class' => 'list-view'];
-    /**
-     * @var callable|null ?Closure an anonymous function that is called once BEFORE rendering each data model.
-     *              It should have the following signature:
-     * ```php
-     * function ($model, $key, $index, $widget)
-     * ```
-     * - `$model`: the current data model being rendered
-     * - `$key`: the key value associated with the current data model
-     * - `$index`: the zero-based index of the data model in the model array returned by [[dataProvider]]
-     * - `$widget`: the ListView object
-     * The return result of the function will be rendered directly.
-     * Note: If the function returns `null`, nothing will be rendered before the item.
-     *
-     * @see renderBeforeItem
-     */
+    /** @var array|Closure */
+    private $itemOptions = [];
+    /** @var callable|string|null */
+    private $itemView;
+    private string $viewPath = '';
+    private array $viewParams = [];
+    private string $separator = "\n";
+    /** @var callable|null */
     private $beforeItem;
-    /**
-     * @var callable|null ?Closure an anonymous function that is called once AFTER rendering each data model.
-     * It should have the same signature as [[beforeItem]].
-     * The return result of the function will be rendered directly.
-     * Note: If the function returns `null`, nothing will be rendered after the item.
-     *
-     * @see renderAfterItem
-     */
+    /** @var callable|null */
     private $afterItem;
+    private Aliases $aliases;
+    private WebView $webView;
+
+    public function __construct(Aliases $aliases, TranslatorInterface $translator, WebView $webView)
+    {
+        $this->aliases = $aliases;
+        $this->webView = $webView;
+
+        parent::__construct($translator);
+    }
 
     /**
      * Renders all data models.
      *
-     * @throws \Throwable
+     * @throws Throwable
      *
      * @return string the rendering result
      */
     public function renderItems(): string
     {
-        $models = $this->getDataReader()->read();
+        $models = $this->getDataReader();
         $keys = array_keys($models);
         $rows = [];
+
         foreach (array_values($models) as $index => $model) {
             $key = $keys[$index];
             if (($before = $this->renderBeforeItem($model, $key, $index)) !== null) {
@@ -120,42 +85,46 @@ class ListView extends BaseListView implements ViewContextInterface
     }
 
     /**
-     * Calls [[beforeItem]] closure, returns execution result.
-     * If [[beforeItem]] is not a closure, `null` will be returned.
+     * Calls {@see beforeItem} closure, returns execution result.
+     *
+     * If {@see beforeItem} is not a closure, `null` will be returned.
      *
      * @param mixed $model the data model to be rendered
      * @param mixed $key the key value associated with the data model
-     * @param int $index the zero-based index of the data model in the model array returned by [[dataProvider]].
+     * @param mixed $index the zero-based index of the data model in the model array returned by
+     * {@see PaginatorInterface}.
      *
-     * @return string|null [[beforeItem]] call result or `null` when [[beforeItem]] is not a closure
+     * @return string|null {@see beforeItem} call result or `null` when {@see beforeItem} is not a closure.
      *
      * @see beforeItem
      */
     protected function renderBeforeItem($model, $key, $index): ?string
     {
         if ($this->beforeItem instanceof Closure) {
-            return \call_user_func($this->beforeItem, $model, $key, $index, $this);
+            return call_user_func($this->beforeItem, $model, $key, $index, $this);
         }
 
         return null;
     }
 
     /**
-     * Calls [[afterItem]] closure, returns execution result.
-     * If [[afterItem]] is not a closure, `null` will be returned.
+     * Calls {@see afterItem} closure, returns execution result.
      *
-     * @param mixed $model the data model to be rendered
-     * @param mixed $key the key value associated with the data model
-     * @param int $index the zero-based index of the data model in the model array returned by [[dataProvider]].
+     * If {@see afterItem} is not a closure, `null` will be returned.
      *
-     * @return string|null [[afterItem]] call result or `null` when [[afterItem]] is not a closure
+     * @param mixed $model the data model to be rendered.
+     * @param mixed $key the key value associated with the data model.
+     * @param mixed $index the zero-based index of the data model in the model array returned by
+     * {@see PaginatorInterface}.
+     *
+     * @return string|null {@see afterItem} call result or `null` when {@see afterItem} is not a closure
      *
      * @see afterItem
      */
     protected function renderAfterItem($model, $key, $index): ?string
     {
         if ($this->afterItem instanceof Closure) {
-            return \call_user_func($this->afterItem, $model, $key, $index, $this);
+            return call_user_func($this->afterItem, $model, $key, $index, $this);
         }
 
         return null;
@@ -166,9 +135,10 @@ class ListView extends BaseListView implements ViewContextInterface
      *
      * @param mixed $model the data model to be rendered
      * @param mixed $key the key value associated with the data model
-     * @param int $index the zero-based index of the data model in the model array returned by [[dataProvider]].
+     * @param mixed $index the zero-based index of the data model in the model array returned by
+     * {@see PaginatorInterface}.
      *
-     * @throws \Throwable
+     * @throws Throwable
      *
      * @return string the rendering result
      */
@@ -176,9 +146,9 @@ class ListView extends BaseListView implements ViewContextInterface
     {
         if ($this->itemView === null) {
             $content = (string) $key;
-        } elseif (\is_string($this->itemView)) {
-            $content = $this->getView()->render(
-                $this->getAliases()->get($this->getItemViewPath()),
+        } elseif (is_string($this->itemView)) {
+            $content = $this->webView->render(
+                $this->getItemViewPath(),
                 array_merge(
                     [
                         'model' => $model,
@@ -190,19 +160,19 @@ class ListView extends BaseListView implements ViewContextInterface
                 ),
                 $this
             );
-        } elseif (\is_callable($this->itemView)) {
-            $content = \call_user_func($this->itemView, $model, $key, $index, $this);
+        } elseif (is_callable($this->itemView)) {
+            $content = call_user_func($this->itemView, $model, $key, $index, $this);
         } else {
             throw new InvalidConfigException('Unknown type of $itemView');
         }
         if ($this->itemOptions instanceof Closure) {
-            $options = \call_user_func($this->itemOptions, $model, $key, $index, $this);
+            $options = call_user_func($this->itemOptions, $model, $key, $index, $this);
         } else {
             $options = $this->itemOptions;
         }
 
         $tag = ArrayHelper::remove($options, 'tag', 'div');
-        $options['data-key'] = \is_array($key) ? json_encode(
+        $options['data-key'] = is_array($key) ? json_encode(
             $key,
             JSON_THROW_ON_ERROR | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE
         ) : (string)$key;
@@ -210,76 +180,191 @@ class ListView extends BaseListView implements ViewContextInterface
         return Html::tag($tag, $content, $options);
     }
 
-    public function itemOptions($itemOptions): self
+    /**
+     * Returns by default the base path configured in the view.
+     *
+     * @return string
+     */
+    public function getViewPath(): string
     {
-        if (\is_array($itemOptions)) {
-            $this->itemOptions = ArrayHelper::merge($this->itemOptions, $itemOptions);
+        $path = $this->webView->getBasePath();
 
-            return $this;
+        if ($this->viewPath !== '') {
+            $path = $this->viewPath;
         }
 
-        if (\is_callable($itemOptions)) {
-            $this->itemOptions = $itemOptions;
-
-            return $this;
-        }
-
-        throw new \InvalidArgumentException('itemOptions must be either array or callable');
+        return $this->aliases->get($path);
     }
 
     public function getItemViewPath(): ?string
     {
-        if (\is_callable($this->itemView)) {
+        if (is_callable($this->itemView)) {
             return ($this->itemView)();
         }
 
         return $this->itemView;
     }
 
-    public function getViewPath(): string
+    /**
+     * @param callable|null $afterItem Closure an anonymous function that is called once AFTER rendering each data
+     * model.
+     *
+     * It should have the same signature as {@see beforeItem}.
+     *
+     * The return result of the function will be rendered directly.
+     *
+     * Note: If the function returns `null`, nothing will be rendered after the item.
+     *
+     * @return ListView
+     *
+     * @see renderAfterItem
+     */
+    public function withAfterItem(?callable $afterItem): self
     {
-        return '';
+        $new = clone $this;
+        $new->afterItem = $afterItem;
+
+        return $new;
     }
 
     /**
-     * @param callable|string|null $itemView
+     * @param callable|null ?Closure an anonymous function that is called once BEFORE rendering each data model.
      *
-     * @return ListView
+     * It should have the following signature:
+     *
+     * ```php
+     * function ($model, $key, $index, $widget)
+     * ```
+     *
+     * - `$model`: the current data model being rendered
+     * - `$key`: the key value associated with the current data model
+     * - `$index`: the zero-based index of the data model in the model array returned by {@see PaginatorInterface}
+     * - `$widget`: the ListView object
+     *
+     * The return result of the function will be rendered directly.
+     *
+     * Note: If the function returns `null`, nothing will be rendered before the item.
+     *
+     * @return $this
+     *
+     * @see renderBeforeItem
      */
-    public function itemView($itemView): self
+    public function withBeforeItem(?callable $beforeItem): self
     {
-        if ($itemView !== null && !\is_string($itemView) && !\is_callable($itemView)) {
-            throw new \InvalidArgumentException('itemView should be either null, string or callable');
+        $new = clone $this;
+        $new->beforeItem = $beforeItem;
+
+        return $new;
+    }
+
+    /**
+     * @param array|Closure $itemOptions the HTML attributes for the container of the rendering result of each data
+     * model.
+     *
+     * This can be either an array specifying the common HTML attributes for rendering each data item, or an anonymous
+     * function that returns an array of the HTML attributes. The anonymous function will be called once for every data
+     * model returned by {@see PaginatorInterface]]. The "tag" element specifies the tag name of the container element
+     * and defaults to "div". If "tag" is false, it means no container element will be rendered. If this property is
+     * specified as an anonymous function, it should have the following signature:
+     *
+     * ```php
+     * function ($model, $key, $index, $widget)
+     * ```
+     *
+     * @return $this
+     *
+     * {@see Html::renderTagAttributes()} for details on how attributes are being rendered.
+     */
+    public function withItemOptions($itemOptions): self
+    {
+        $new = clone $this;
+
+        if (is_array($itemOptions)) {
+            $new->itemOptions = ArrayHelper::merge($this->itemOptions, $itemOptions);
+
+            return $new;
         }
 
-        $this->itemView = $itemView;
+        if (is_callable($itemOptions)) {
+            $new->itemOptions = $itemOptions;
 
-        return $this;
+            return $new;
+        }
+
+        throw new InvalidArgumentException('itemOptions must be either array or callable');
     }
 
-    public function separator(string $separator): self
+    /**
+     * @param callable|string|null $itemView the name of the view for rendering each data item, or a callback (e.g. an
+     * anonymous function) for rendering each data item. If it specifies a view name, the following variables will be
+     * available in the view:
+     *
+     * - `$model`: mixed, the data model.
+     * - `$key`: mixed, the key value associated with the data item.
+     * - `$index`: integer, the zero-based index of the data item in the items array returned by
+     * {@see PaginatorInterface}.
+     * - `$widget`: ListView, this widget instance.
+     *
+     * Note that the view name is resolved into the view file by the current context of the {@see view} object.
+     *
+     * If this property is specified as a callback, it should have the following signature:
+     * ```php
+     * function ($model, $key, $index, $widget)
+     * ```
+     *
+     * @return $this
+     */
+    public function withItemView($itemView): self
     {
-        $this->separator = $separator;
+        $new = clone $this;
 
-        return $this;
+        if ($itemView !== null && !is_string($itemView) && !is_callable($itemView)) {
+            throw new InvalidArgumentException('itemView should be either null, string or callable');
+        }
+
+        $new->itemView = $itemView;
+
+        return $new;
     }
 
-    public function beforeItem(?callable $beforeItem): self
+    /**
+     * @param string $separator the HTML code to be displayed between any two consecutive items.
+     *
+     * @return $this
+     */
+    public function withSeparator(string $separator): self
     {
-        $this->beforeItem = $beforeItem;
+        $new = clone $this;
+        $new->separator = $separator;
 
-        return $this;
+        return $new;
     }
 
-    public function afterItem(?callable $afterItem): self
+    /**
+     * @param string $viewPath set view path for {@see ListView}.
+     *
+     * @return $this
+     */
+    public function withViewPath(string $viewPath): self
     {
-        $this->afterItem = $afterItem;
+        $new = clone $this;
+        $new->viewPath = $viewPath;
 
-        return $this;
+        return $new;
     }
 
-    public function getId(): string
+    /**
+     * @param array $viewParams additional parameters to be passed to {@see itemView} when it is being rendered.
+     *
+     * This property is used only when {@see itemView} is a string representing a view name.
+     *
+     * @return $this
+     */
+    public function withViewParams(array $viewParams): self
     {
-        return 'listview-widget-1';
+        $new = clone $this;
+        $new->viewParams = $viewParams;
+
+        return $new;
     }
 }
