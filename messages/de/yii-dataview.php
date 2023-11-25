5d60ce1cb4cdc432bba757d5282c54c13efb79b0
Guido Pannenbecker <info@sd-gp.de>
2023-11-25 19:30:48 +0100

    Add german translations

---
+++ messages/de/yii-dataview.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'No results found.' => 'Keine Ergebnisse gefunden.',
+    'Page <b>{currentPage}</b> of <b>{totalPages}</b>' => 'Seite <b>{currentPage}</b> von <b>{totalPages}</b>',
+    'Actions' => 'Aktionen',
+];
