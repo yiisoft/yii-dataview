19097181a12e7e21581d5bedd65b0b25c4f516dc
Sergei Predvoditelev <sergei@predvoditelev.ru>
2023-12-05 21:19:22 +0300

    improve theme

--- config/widgets-themes.php
+++ config/widgets-themes.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use Yiisoft\Yii\DataView\Column\ActionColumn;
 use Yiisoft\Yii\DataView\GridView;
 
 return [
@@ -9,6 +10,13 @@ return [
         GridView::class => [
             'tableClass()' => ['table table-bordered'],
             'tbodyClass()' => ['table-group-divider'],
+            'columnsConfigs()' => [
+                [
+                    ActionColumn::class => [
+                        'buttonClass' => 'btn btn-outline-secondary',
+                    ],
+                ]
+            ],
         ],
     ],
 ];
