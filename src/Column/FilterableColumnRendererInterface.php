dcc852623a5efb8c6a2496ad49b78b0312bf3e53
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-01-22 17:03:43 +0300

    Replace `checkColumn()` methods in column renderers to generic

--- src/Column/FilterableColumnRendererInterface.php
+++ src/Column/FilterableColumnRendererInterface.php
@@ -10,12 +10,21 @@ use Yiisoft\Yii\DataView\Column\Base\FilterContext;
 use Yiisoft\Yii\DataView\Column\Base\MakeFilterContext;
 use Yiisoft\Yii\DataView\Filter\Factory\IncorrectValueException;
 
+/**
+ * @template TColumn as ColumnInterface
+ * @extends ColumnRendererInterface<TColumn>
+ */
 interface FilterableColumnRendererInterface extends ColumnRendererInterface
 {
+    /**
+     * @psalm-param TColumn $column
+     */
     public function renderFilter(ColumnInterface $column, Cell $cell, FilterContext $context): ?Cell;
 
     /**
      * @throws IncorrectValueException
+     *
+     * @psalm-param TColumn $column
      */
     public function makeFilter(ColumnInterface $column, MakeFilterContext $context): ?FilterInterface;
 }
--- src/Column/SerialColumnRenderer.php
+++ src/Column/SerialColumnRenderer.php
@@ -4,30 +4,28 @@ declare(strict_types=1);
 
 namespace Yiisoft\Yii\DataView\Column;
 
-use InvalidArgumentException;
 use Yiisoft\Yii\DataView\Column\Base\Cell;
 use Yiisoft\Yii\DataView\Column\Base\DataContext;
 use Yiisoft\Yii\DataView\Column\Base\GlobalContext;
 use Yiisoft\Yii\DataView\Column\Base\HeaderContext;
 
+/**
+ * @implements ColumnRendererInterface<SerialColumn>
+ */
 final class SerialColumnRenderer implements ColumnRendererInterface
 {
     public function renderColumn(ColumnInterface $column, Cell $cell, GlobalContext $context): Cell
     {
-        $this->checkColumn($column);
         return $cell->addAttributes($column->columnAttributes);
     }
 
     public function renderHeader(ColumnInterface $column, Cell $cell, HeaderContext $context): Cell
     {
-        $this->checkColumn($column);
         return $cell->content($column->header ?? '#');
     }
 
     public function renderBody(ColumnInterface $column, Cell $cell, DataContext $context): Cell
     {
-        $this->checkColumn($column);
-
         return $cell
             ->addAttributes($column->bodyAttributes)
             ->content((string)($context->index + 1));
@@ -35,23 +33,6 @@ final class SerialColumnRenderer implements ColumnRendererInterface
 
     public function renderFooter(ColumnInterface $column, Cell $cell, GlobalContext $context): Cell
     {
-        $this->checkColumn($column);
         return $cell->content($column->footer ?? '');
-    }
-
-    /**
-     * @psalm-assert SerialColumn $column
-     */
-    private function checkColumn(ColumnInterface $column): void
-    {
-        if (!$column instanceof SerialColumn) {
-            throw new InvalidArgumentException(
-                sprintf(
-                    'Expected "%s", but "%s" given.',
-                    SerialColumn::class,
-                    $column::class
-                )
-            );
-        }
     }
 }
