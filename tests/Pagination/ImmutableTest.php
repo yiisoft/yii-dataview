e76e61b98a4e40363c4c298c26ea7cc226578dcc
Wilmer Arambula <terabytesoftw@gmail.com>
2025-04-14 06:27:03 -0400

    Raise code coverage `OffsetPagination` class.

--- tests/Pagination/ImmutableTest.php
+++ tests/Pagination/ImmutableTest.php
@@ -15,6 +15,16 @@ final class ImmutableTest extends TestCase
     public function testOffsetPagination(): void
     {
         $offsetPagination = OffsetPagination::widget();
+        $this->assertNotSame($offsetPagination, $offsetPagination->addLinkClass(''));
+        $this->assertNotSame($offsetPagination, $offsetPagination->containerAttributes([]));
+        $this->assertNotSame($offsetPagination, $offsetPagination->containerTag('div'));
+        $this->assertNotSame($offsetPagination, $offsetPagination->currentLinkClass(''));
+        $this->assertNotSame($offsetPagination, $offsetPagination->disabledLinkClass(''));
+        $this->assertNotSame($offsetPagination, $offsetPagination->labelFirst(''));
+        $this->assertNotSame($offsetPagination, $offsetPagination->labelLast(''));
+        $this->assertNotSame($offsetPagination, $offsetPagination->labelNext(''));
+        $this->assertNotSame($offsetPagination, $offsetPagination->labelPrevious(''));
+        $this->assertNotSame($offsetPagination, $offsetPagination->linkClass(''));
         $this->assertNotSame($offsetPagination, $offsetPagination->maxNavLinkCount(10));
     }
 }
