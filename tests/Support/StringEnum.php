a164767c0be62262f841c4d0a84eee65171a99a2
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-06-05 16:25:45 +0300

    Add `class()` and `addClass()` methods to `DropdownFilter`

---
+++ tests/Support/StringEnum.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Yii\DataView\Tests\Support;
+
+enum StringEnum: string
+{
+    case TEST_CLASS_1 = 'test-class-1';
+    case TEST_CLASS_2 = 'test-class-2';
+}
+
