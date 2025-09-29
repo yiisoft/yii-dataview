6d08166b59ec6f22df61b4255ad83f015854476c
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-09-29 11:15:02 +0300

    ActionColumn

---
+++ tests/Support/SimpleActionColumnUrlCreator.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Yii\DataView\Tests\Support;
+
+use Yiisoft\Yii\DataView\GridView\Column\ActionColumn;
+use Yiisoft\Yii\DataView\GridView\Column\Base\DataContext;
+
+use function is_object;
+
+final class SimpleActionColumnUrlCreator
+{
+    public function __construct(
+        private readonly string $primaryKey = 'id',
+    )
+    {
+    }
+
+    public function __invoke(string $action, DataContext $context): string
+    {
+        /** @var ActionColumn $column */
+        $column = $context->column;
+
+        /** @var array $config */
+        $config = $column->urlConfig;
+
+        $primaryKey = $config['primaryKey'] ?? $this->primaryKey;
+
+        $primaryKeyValue = is_object($context->data)
+            ? $context->data->$primaryKey
+            : $context->data[$primaryKey];
+
+        return '/' . $action . '/' . $primaryKeyValue;
+    }
+}
