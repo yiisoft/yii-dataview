1953b4b0b546fe53b3359eb0cc8de707685dba35
Sergei Predvoditelev <sergei@predvoditelev.ru>
2026-08-13 17:23:43 +0300

    fix

--- src/BaseListView.php
+++ src/BaseListView.php
@@ -204,6 +204,7 @@ abstract class BaseListView extends Widget
      *
      * @throws LogicException If pagination is disabled and the configured data reader is a paginator.
      * @throws PageNotFoundException
+     * @throws InvalidPageException
      */
     final public function prepareDataReader(bool $withPagination = true): ?ReadableDataInterface
     {
